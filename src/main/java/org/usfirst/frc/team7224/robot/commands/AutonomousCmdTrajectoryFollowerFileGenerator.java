77da67ad18b4e7e42030cfe370c2ccd869cefaa8
Isaiah Shurtliff <isaiah.shurtliff@keysight.com>
2021-03-30 15:05:12 -0400

    now builds

--- src/main/java/org/usfirst/frc/team7224/robot/commands/AutonomousCmdTrajectoryFollowerFileGenerator.java
+++ src/main/java/org/usfirst/frc/team7224/robot/commands/AutonomousCmdTrajectoryFollowerFileGenerator.java
@@ -9,7 +9,6 @@ import org.usfirst.frc.team7224.robot.RobotConstants;
 import edu.wpi.first.wpilibj.command.Command;
 import org.usfirst.frc.team7224.robot.Point;
 
-
 import jaci.pathfinder.Pathfinder;
 import jaci.pathfinder.Trajectory;
 import jaci.pathfinder.Waypoint;
@@ -17,7 +16,6 @@ import jaci.pathfinder.followers.EncoderFollower;
 import jaci.pathfinder.modifiers.TankModifier;
 
 public class AutonomousCmdTrajectoryFollowerFileGenerator extends Command {
-
 
   edu.wpi.first.wpilibj.Timer timeout;
   Timer t;
@@ -32,12 +30,10 @@ public class AutonomousCmdTrajectoryFollowerFileGenerator extends Command {
 
   String ScenarioName;
 
-  
-
   public AutonomousCmdTrajectoryFollowerFileGenerator(List<Point> xyPoints, String scenarioName) {
     requires(Robot.chassis);
     for (Point point : xyPoints) {
-      waypoints.add(point.X * inchesToMeter, point.Y * inchesToMeter, point.D);
+      waypoints.add(new Waypoint(point.X * inchesToMeter, point.Y * inchesToMeter, point.D));
     }
     ScenarioName = scenarioName;
   }
@@ -70,8 +66,8 @@ public class AutonomousCmdTrajectoryFollowerFileGenerator extends Command {
     // new Waypoint(0,0, Pathfinder.d2r(0)),
     // new Waypoint(5,0, Pathfinder.d2r(0))
     // };
-    for (int i = 0, j = 0; i < waypoints.size() - 1; i++,j++) {
-      Waypoint[] points = waypoints.subList(i, i+1).toArray(Waypoint[] :: new);
+    for (int i = 0, j = 0; i < waypoints.size() - 1; i++, j++) {
+      Waypoint[] points = waypoints.subList(i, i + 1).toArray(Waypoint[]::new);
       // SmartDashboard.putNumber("files writer2", 0);
 
       // Trajectory.Config config = new
@@ -91,118 +87,118 @@ public class AutonomousCmdTrajectoryFollowerFileGenerator extends Command {
       // Trajectory.Config config = new
       // Trajectory.Config(Trajectory.FitMethod.HERMITE_CUBIC,
       // Trajectory.Config.SAMPLES_LOW,0.05, .35, .3, .4);
-      Trajectory.Config config = new Trajectory.Config(Trajectory.FitMethod.HERMITE_CUBIC, Trajectory.Config.SAMPLES_LOW,
-          0.05, 1.2, .5, .4);
+      Trajectory.Config config = new Trajectory.Config(Trajectory.FitMethod.HERMITE_CUBIC,
+          Trajectory.Config.SAMPLES_LOW, 0.05, 1.2, .5, .4);
 
       Trajectory trajectory = Pathfinder.generate(points, config);
       File myFile = new File(String.format("/home/lvuser/%s%d.csv", j));
       Pathfinder.writeToCSV(myFile, trajectory);
-    }
 
-    double wheelbase_width = .61; // MG updated
+      double wheelbase_width = .61; // MG updated
 
-    // Create the Modifier Object
-    TankModifier modifier = new TankModifier(trajectory).modify(wheelbase_width);
+      // Create the Modifier Object
+      TankModifier modifier = new TankModifier(trajectory).modify(wheelbase_width);
 
-    left = new EncoderFollower(modifier.getLeftTrajectory());
-    right = new EncoderFollower(modifier.getRightTrajectory());
+      left = new EncoderFollower(modifier.getLeftTrajectory());
+      right = new EncoderFollower(modifier.getRightTrajectory());
 
-    // Encoder Position is the current, cumulative position of your encoder.
-    // If you're using an SRX, this will be the
-    // 'getEncPosition' function.
-    // 100 is the amount of encoder ticks per full revolution
-    // 20 ticks per rev * 5:1 gear ratio = 100
-    // Wheel Diameter is the diameter of your wheels (or pulley for a track system)
-    // in meters
-    // 6" * .0254 = .1016
+      // Encoder Position is the current, cumulative position of your encoder.
+      // If you're using an SRX, this will be the
+      // 'getEncPosition' function.
+      // 100 is the amount of encoder ticks per full revolution
+      // 20 ticks per rev * 5:1 gear ratio = 100
+      // Wheel Diameter is the diameter of your wheels (or pulley for a track system)
+      // in meters
+      // 6" * .0254 = .1016
 
-    left.configureEncoder(Robot.chassis.getLeftEncoderPosition(), 365, 0.1524);
-    right.configureEncoder(Robot.chassis.getRightEncoderPosition(), 365, 0.1525);
+      left.configureEncoder(Robot.chassis.getLeftEncoderPosition(), 365, 0.1524);
+      right.configureEncoder(Robot.chassis.getRightEncoderPosition(), 365, 0.1525);
 
-    // The first argument is the proportional gain. Usually this will be quite high
-    // The second argument is the integral gain. This is unused for motion profiling
-    // The third argument is the derivative gain. Tweak this if you are unhappy with
-    // the tracking of the trajectory
-    // The fourth argument is the velocity ratio. This is 1 over the maximum
-    // velocity you provided in the
-    // trajectory configuration (it translates m/s to a -1 to 1 scale that your
-    // motors can read)
-    // The fifth argument is your acceleration gain. Tweak this if you want to get
-    // to a higher or lower speed quicker
-    left.configurePIDVA(0.7, 0.0, 0.00, 0.6, 0);
-    right.configurePIDVA(0.7, 0.0, 0.00, 0.6, 0);
+      // The first argument is the proportional gain. Usually this will be quite high
+      // The second argument is the integral gain. This is unused for motion profiling
+      // The third argument is the derivative gain. Tweak this if you are unhappy with
+      // the tracking of the trajectory
+      // The fourth argument is the velocity ratio. This is 1 over the maximum
+      // velocity you provided in the
+      // trajectory configuration (it translates m/s to a -1 to 1 scale that your
+      // motors can read)
+      // The fifth argument is your acceleration gain. Tweak this if you want to get
+      // to a higher or lower speed quicker
+      left.configurePIDVA(0.7, 0.0, 0.00, 0.6, 0);
+      right.configurePIDVA(0.7, 0.0, 0.00, 0.6, 0);
 
-    t = new Timer();
-    Robot.chassis.brakemode(true);
-    Robot.chassis.resetgyro();
-    t.schedule(new TimerTask() {
-      // Sample setup
-      // double l = left.calculate(encoder_position_left);
-      // double r = right.calculate(encoder_position_right);
-      //
-      // double gyro_heading = ... your gyro code here ... // Assuming the gyro is
-      // giving a value in degrees
-      // double desired_heading = Pathfinder.r2d(left.getHeading()); // Should also be
-      // in degrees
-      //
-      // double angleDifference = Pathfinder.boundHalfDegrees(desired_heading -
-      // gyro_heading);
-      // double turn = 0.8 * (-1.0/80.0) * angleDifference;
-      //
-      // setLeftMotors(l + turn);
-      // setRightMotors(r - turn);
-      ////// End sample setup
+      t = new Timer();
+      Robot.chassis.brakemode(true);
+      Robot.chassis.resetgyro();
+      t.schedule(new TimerTask() {
+        // Sample setup
+        // double l = left.calculate(encoder_position_left);
+        // double r = right.calculate(encoder_position_right);
+        //
+        // double gyro_heading = ... your gyro code here ... // Assuming the gyro is
+        // giving a value in degrees
+        // double desired_heading = Pathfinder.r2d(left.getHeading()); // Should also be
+        // in degrees
+        //
+        // double angleDifference = Pathfinder.boundHalfDegrees(desired_heading -
+        // gyro_heading);
+        // double turn = 0.8 * (-1.0/80.0) * angleDifference;
+        //
+        // setLeftMotors(l + turn);
+        // setRightMotors(r - turn);
+        ////// End sample setup
 
-      public void run() {
-        lasttime = edu.wpi.first.wpilibj.Timer.getFPGATimestamp();
-        timelapse = edu.wpi.first.wpilibj.Timer.getFPGATimestamp() - lasttime;
-        if (!left.isFinished() || !right.isFinished()) {
+        public void run() {
+          lasttime = edu.wpi.first.wpilibj.Timer.getFPGATimestamp();
+          timelapse = edu.wpi.first.wpilibj.Timer.getFPGATimestamp() - lasttime;
+          if (!left.isFinished() || !right.isFinished()) {
 
-          double l = left.calculate(Robot.chassis.getLeftEncoderPosition());
-          double r = right.calculate(Robot.chassis.getRightEncoderPosition());
-          double gyro_heading = -Robot.chassis.getGyroAngle(); // Assuming the gyro is giving a value in degrees
-          double desired_heading = Pathfinder.r2d(left.getHeading()); // Should also be in degrees
-          double angleDifference = Pathfinder.boundHalfDegrees(desired_heading - gyro_heading);
-          double turn = 0.8 * (-1.0 / 80.0) * angleDifference;
-          Robot.chassis.tankDrive((l + turn), (r - turn));
-          /*
-           * Robot.chassis.tankDrive(-(l + turn),-(r - turn));
-           * SmartDashboard.putNumber("tra head", desired_heading);
-           * SmartDashboard.putNumber("tra angle Difference", angleDifference);
-           * SmartDashboard.putNumber("tra gyro 2", -Robot.chassis.getGyroAngle());
-           * SmartDashboard.putNumber("tra right", r);
-           * SmartDashboard.putNumber("tra left", l); SmartDashboard.putNumber("Turn",
-           * turn); SmartDashboard.putDouble("tra right", -(r - turn));
-           * SmartDashboard.putDouble("tra left", -(l + turn));
-           * SmartDashboard.putNumber("tra encoder right",
-           * Robot.chassis.getRightEncoderPosition());
-           * SmartDashboard.putNumber("tra encodeer left",
-           * Robot.chassis.getLeftEncoderPosition());
-           */
+            double l = left.calculate(Robot.chassis.getLeftEncoderPosition());
+            double r = right.calculate(Robot.chassis.getRightEncoderPosition());
+            double gyro_heading = -Robot.chassis.getGyroAngle(); // Assuming the gyro is giving a value in degrees
+            double desired_heading = Pathfinder.r2d(left.getHeading()); // Should also be in degrees
+            double angleDifference = Pathfinder.boundHalfDegrees(desired_heading - gyro_heading);
+            double turn = 0.8 * (-1.0 / 80.0) * angleDifference;
+            Robot.chassis.tankDrive((l + turn), (r - turn));
+            /*
+             * Robot.chassis.tankDrive(-(l + turn),-(r - turn));
+             * SmartDashboard.putNumber("tra head", desired_heading);
+             * SmartDashboard.putNumber("tra angle Difference", angleDifference);
+             * SmartDashboard.putNumber("tra gyro 2", -Robot.chassis.getGyroAngle());
+             * SmartDashboard.putNumber("tra right", r);
+             * SmartDashboard.putNumber("tra left", l); SmartDashboard.putNumber("Turn",
+             * turn); SmartDashboard.putDouble("tra right", -(r - turn));
+             * SmartDashboard.putDouble("tra left", -(l + turn));
+             * SmartDashboard.putNumber("tra encoder right",
+             * Robot.chassis.getRightEncoderPosition());
+             * SmartDashboard.putNumber("tra encodeer left",
+             * Robot.chassis.getLeftEncoderPosition());
+             */
 
-        } else {
-          Robot.chassis.tankDrive(0, 0);
-          left.reset();
-          right.reset();
+          } else {
+            Robot.chassis.tankDrive(0, 0);
+            left.reset();
+            right.reset();
+          }
+          // SmartDashboard.putNumber("tra gyro 2", Robot.chassis.getGyroAngle());
+          // System.out.println("r "+ r);
+          // System.out.println("l "+ l);
+          // SmartDashboard.putNumber("tra encoder right",
+          // Robot.chassis.getRightEncoderPosition());
+          // SmartDashboard.putNumber("tra encodeer left",
+          // Robot.chassis.getLeftEncoderPosition());
+          // SmartDashboard.putNumber("time lapse", timelapse);
+          // System.out.println(" Run specific task at given time." +
+          // System.currentTimeMillis());
+          // System.out.println(" Total time." + RobotConstants.TrajectorySegments*.05);
         }
-        // SmartDashboard.putNumber("tra gyro 2", Robot.chassis.getGyroAngle());
-        // System.out.println("r "+ r);
-        // System.out.println("l "+ l);
-        // SmartDashboard.putNumber("tra encoder right",
-        // Robot.chassis.getRightEncoderPosition());
-        // SmartDashboard.putNumber("tra encodeer left",
-        // Robot.chassis.getLeftEncoderPosition());
-        // SmartDashboard.putNumber("time lapse", timelapse);
-        // System.out.println(" Run specific task at given time." +
-        // System.currentTimeMillis());
-        // System.out.println(" Total time." + RobotConstants.TrajectorySegments*.05);
-      }
 
-    }, 0, 50); // end timed task 0 delay, execute every 50 mSec
-    left.reset();
-    right.reset();
-    Robot.chassis.resetEncoders();
-    Robot.chassis.displayChasisData();
+      }, 0, 50); // end timed task 0 delay, execute every 50 mSec
+      left.reset();
+      right.reset();
+      Robot.chassis.resetEncoders();
+      Robot.chassis.displayChasisData();
+    }
 
   }
 
